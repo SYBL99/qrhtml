58d4b243de4b96b8fdf16cf33eef77dc32295e11
Dmitriy <dmitriy.shupikov@gmail.com>
2023-04-26 16:47:53 +0700

    dialog

--- src/Qr.js
+++ src/Qr.js
@@ -2,51 +2,67 @@ import { Html5QrcodeScanner, Html5Qrcode } from 'html5-qrcode';
 import './App.css';
 import { useEffect, useState } from 'react';
 
-// Creates the configuration object for Html5QrcodeScanner.
-
+// Creates the configuration object for Html5QrcodeScanner
 const Html5QrcodePlugin = (props) => {
+    const [camera, setCamera] = useState(null)
+
+
+
+
     const qrcodeRegionId = "html5qr";
-    const output = document.querySelector("#output")
-    const handleResolve = (decodedText, decodedResult) => {
-        const node = document.createElement("div");
-        const textnode = document.createTextNode(decodedText);
-        node.appendChild(textnode);
-        document.getElementById("output").appendChild(node);
-    }
 
     useEffect(() => {
-
+        
         Html5Qrcode.getCameras().then(devices => {
             /**
              * devices would be an array of objects of type:
              * { id: "id", label: "label" }
              */
             if (devices && devices.length) {
-              var cameraId = devices[0].id;
+              var cameraId = devices[devices.length - 1].id; // потом заменить на фильтр фронталки
               const html5QrCode = new Html5Qrcode(/* element id */ qrcodeRegionId);
                 html5QrCode.start(
                 cameraId, 
                 {
-                    fps: 10,    // Optional, frame per seconds for qr code scanning
-                    qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
+                    fps: 10,   
+                    qrbox: { width: 200, height: 200 }
                 },
-                handleResolve,
+                (decodedText, decodedResult) => { html5QrCode.stop(); handleResolve(decodedText, decodedResult) },
                 (errorMessage) => {
                     // parse error, ignore it.
                 })
                 .catch((err) => {
                 // Start failed, handle it.
-                });
+                })
+                ;
             }
           }).catch(err => {
             // handle err
           });
+
+          const handleResolve = (decodedText, decodedResult) => {
+            const node = document.createElement("div");
+            const textnode = document.createTextNode(decodedText);
+            node.appendChild(textnode);
+            document.getElementById("output").appendChild(node);
+    
+            const dialog = document.getElementById("show_qr");
+            console.log(decodedResult)
+            dialog.firstChild.innerText = decodedText
+            dialog.showModal()
+
+        }
+
     }, []);
 
     return (
         <>
-            <div id={qrcodeRegionId}/>
+            <div className='Qr_container' id={qrcodeRegionId}/>
             <div id='output'> Сюда вывод</div>
+            <dialog className='Qr_container' id='show_qr'>
+                <p>Должен быть QR</p>
+                <button onClick={(event)=>{event.target.closest('dialog').close()}}>close</button>
+                </dialog>
         </>
 
     );
